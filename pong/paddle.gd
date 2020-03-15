4275a4410569a3f528eae6201b9bbd6bc50c9044
AsynchronousGillz <vananger93@gmail.com>
2020-03-15 12:02:05 -0600

    godot fun and games

---
+++ pong/paddle.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+# Called when the node enters the scene tree for the first time.
+const SPEED = 200
+var motion = Vector2()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+	else:
+		motion.x = 0
